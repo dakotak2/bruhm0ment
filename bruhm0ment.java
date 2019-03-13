b3194380651e10544299068a94f8369e71df8228
dakotak2 <dakota.k316@gmail.com>
2019-03-13 12:38:10 -0700

    Initial sharing of project

---
+++ bruhm0ment.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class bruhm0ment here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class bruhm0ment
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class bruhm0ment
+     */
+    public bruhm0ment()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
